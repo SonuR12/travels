ceea28a858d83dbaacc4702fa59daff9d89e27d8
SonuR12 <sonurai7011@gmail.com>
2025-05-09 23:04:21 +0530

    pt-16 add

--- src/components/home/page.tsx
+++ src/components/home/page.tsx
@@ -6,12 +6,12 @@ import React from "react";
 
 export default function HomePage(){
   return (
-    <>
+    <div className="pt-16">
       <HeroSection />
       {/* <ContactForm /> */}
       <ImageSection />
       <TestimonialsPage />
       <Footer />
-    </>
+    </div>
   );
 };
